50e5f98330d3c5d9ae71499e1bd6639bed1bd040
RC_Student_lab <RC_Student_lab@labG9AEB3.phdvvtwzx4oudioi2mg3lhtmwg.fx.internal.cloudapp.net>
2024-10-15 16:37:52 +0000

    Vowel Count

---
+++ src/main/java/com/mycompany/vowelcount/VowelCount.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.vowelcount;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class VowelCount {
+
+    public static void main(String[] args) {
+        ;
+    }
+}
